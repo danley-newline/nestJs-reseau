9a3d4a45160d843a36cf11ea711b48070e192882
daniel kacou <danleynewline@gmail.com>
2023-07-22 22:29:58 +0000

    avant protection de route

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,108 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import * as speakeasy from 'speakeasy';
+import { MailerService } from 'src/mailer/mailer.service';
+import { SignupDto } from './dto/signupDto';
+import { SigninDto } from './dto/signinDto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { ResetPasswordDemandDto } from './dto/resetPasswordDemandDto';
+import { ResetPasswordConfirmationDto } from './dto/resetPasswordConfirmationDto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly mailerService: MailerService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async signup(signupDto: SignupDto) {
+    const { email, username, password } = signupDto;
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    if (user) throw new ConflictException('User already exists');
+    const hash = await bcrypt.hash(password, 10);
+    await this.prismaService.user.create({
+      data: { email, username, password: hash },
+    });
+
+    await this.mailerService.sendSignupConfirmation(email);
+
+    return { data: 'User succesfully created' };
+  }
+
+  async signin(signinDto: SigninDto) {
+    const { email, password } = signinDto;
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    if (!user) throw new NotFoundException('User not found');
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) throw new UnauthorizedException('Password not match');
+
+    const payload = {
+      sub: user.userId,
+      email: user.email,
+    };
+
+    const token = this.jwtService.sign(payload, {
+      expiresIn: '2h',
+      secret: this.configService.get('SECRET_KEY'),
+    });
+    return {
+      token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    };
+  }
+
+  async resetPasswordDemand(resetPasswordDemandDto: ResetPasswordDemandDto) {
+    const { email } = resetPasswordDemandDto;
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    if (!user) throw new NotFoundException('User not found');
+    const code = speakeasy.totp({
+      secret: this.configService.get('OTP_CODE'),
+      digits: 5,
+      step: 60 * 15,
+      encoding: 'base32',
+    });
+
+    const url = 'http://localhost:3000/auth/reset-password-confirmation';
+    await this.mailerService.sendResetPassword(email, url, code);
+
+    return { data: 'Reset password mail has been sent' };
+  }
+
+  async resetPasswordConfirmation(
+    resetPasswordConfirmationDto: ResetPasswordConfirmationDto,
+  ) {
+    const { email, password, code } = resetPasswordConfirmationDto;
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    if (!user) throw new NotFoundException('User not found');
+    const match = await speakeasy.totp.verify({
+      secret: this.configService.get('OTP_CODE'),
+      token: code,
+      digits: 5,
+      step: 60 * 15,
+      encoding: 'base32',
+    });
+
+    if (!match) throw new UnauthorizedException('invalid/expiered token ');
+    const hash = await bcrypt.hash(password, 10);
+    await this.prismaService.user.update({
+      where: { email },
+      data: { password: hash },
+    });
+
+    return {
+      data: 'Password updated',
+    };
+  }
+}
